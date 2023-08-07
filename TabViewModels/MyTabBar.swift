8c1e09b1d147910c1cfe1667c7197b8b88f2a07a
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-08-07 15:14:18 -0300

    To add initial commmit

---
+++ TabViewModels/MyTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  MyTabar.swift
+//  TabViewModels
+//
+//  Created by Rafael Gonzaga on 07/08/23.
+//
+
+import SwiftUI
+
+struct MyTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        MyTabBar()
+    }
+}
---
+++ TabViewModels/TabViewModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewModelsApp.swift
+//  TabViewModels
+//
+//  Created by Rafael Gonzaga on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TabViewModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
